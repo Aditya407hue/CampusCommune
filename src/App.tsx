ceb248d93c057220269866f224d56af3e0fa1e9e
Sanu <sanusingh1603@gmail.com>
2025-04-27 16:14:14 +0530

    added pr role in Complete your profile

--- src/App.tsx
+++ src/App.tsx
@@ -164,7 +164,8 @@ function CreateProfile() {
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2"
           >
             <option value="student">Student</option>
-            <option value="admin">Admin</option>
+            {/* <option value="admin">Admin</option> */}
+            <option value="pr">PR</option>
           </select>
         </div>
         <div>
